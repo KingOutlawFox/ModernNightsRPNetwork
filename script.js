775afe99d9db20ff390b5e5413d972ac349e9afb
KingOutlawFox <71665709+KingOutlawFox@users.noreply.github.com>
2023-12-10 13:32:16 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function openLoginModal() {
-  document.getElementById('loginModal').style.display = 'flex';
-}
-
-function openCreateAccountModal() {
-  document.getElementById('createAccountModal').style.display = 'flex';
-}
